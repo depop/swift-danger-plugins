325a7066d99f8d40def7cf1d4a4d719f1baa2b78
Samuel Hervas <samh@depop.com>
2020-03-26 16:09:13 +0000

    Working towards to highlight specific funtions in specific files.

--- Sources/CoverageHighlighter/CoverageHighlighter.swift
+++ Sources/CoverageHighlighter/CoverageHighlighter.swift
@@ -11,6 +11,19 @@ protocol NamedCoverable: Coverable {
     var name: String { get }
 }
 
+public struct Filter {
+    struct FilterFile {
+        let nameSuffix: String
+        let functionPrefix: String?
+    }
+    var filesToFilter: [FilterFile]
+    var filesToExclude: [FilterFile]
+}
+
+extension Filter {
+    var include: [String] { filter.filesToFilter.map { $0.nameSuffix } }
+    var exclude: [String]  { filter.filesToExclude.map { $0.nameSuffix } }
+}
 public class CoverageHighlighter {
 
     class Parser {
@@ -108,26 +121,62 @@ public class CoverageHighlighter {
         let executionCount: Int
     }
 
-    public static func hightlight() -> Void {
+    public static func hightlight(filter: Filter) -> Void {
+
         let danger = Danger()
         let filename = "results.json"
         let parser = Parser()
 
+        let include = filter.include
+        let exclude = filter.exclude
         let result = parser.parse(filename, shouldPrint: false)
-        let toBeTested = result?.targets.map { $0.files }.flatMap { $0 }.filter { ($0.name.contains("Service") || $0.name.contains("Presenter") || $0.name.contains("Manager")) && !$0.name.contains("Test")  }.sorted(by: { $0.lineCoverage > $1.lineCoverage }).map { $0 } ?? [File]()
+
+        let filesToHighlightCoverage = result?.targets.map { $0.files }.flatMap { $0 }.filter { codeFile in
+            include.contains(where: { codeFile.name.contains($0) }) &&
+            !exclude.contains(where: { codeFile.name.contains($0) })
+        }.sorted(by: { $0.lineCoverage > $1.lineCoverage }).map { $0 } ?? [File]()
+
         let modifiedFilesNames = (danger.git.modifiedFiles + danger.git.createdFiles).compactMap { $0.components(separatedBy: "/").last }
 
-        print("Coverage in on the modified Servicies and Presenters")
-
-        toBeTested.filter { file in
+        let modifiedFilesToHightLight = filesToHighlightCoverage.filter { file in
             modifiedFilesNames.contains(file.path.components(separatedBy: "/").last!)
-        }.forEach {
-            let finalMessage = String(format: "%@: %.3f%%", $0.path.components(separatedBy: "/").last!.padding(toLength: 50, withPad: " ", startingAt: 0), $0.lineCoverage * 100)
-            if $0.lineCoverage * 100 < 70 {
+        }
+
+        modifiedFilesToHightLight.forEach { file in
+
+            typealias ResultFilter = (functions: [Filter.FilterFile], files: [Filter.FilterFile])
+
+            let result: ResultFilter  = ([],[])
+
+            let functions: ResultFilter = filter.filesToFilter.reduce(result) { (result, filterFile) -> ResultFilter in
+                var functions = result.functions
+                var files = result.files
+                if filterFile.functionPrefix != nil  && file.name.contains(filterFile.nameSuffix) {
+                    functions.append(filterFile)
+                } else {
+                    files.append(filterFile)
+                }
+                return (functions, files)
+            }
+
+            functions.functions.forEach { function in
+                file.functions.filter { $0.name.contains(function.functionPrefix!) }.forEach {
+                    let finalMessage = String(format: "%@: %.3f%%", $0.name.components(separatedBy: ".").last!.padding(toLength: 50, withPad: " ", startingAt: 0), $0.lineCoverage * 100)
+                    if $0.lineCoverage * 100 < 90 {
+                        warn("Function without test " + finalMessage)
+                    } else {
+                        message("Good test coverage " + finalMessage)
+                    }
+                }
+            }
+
+            let finalMessage = String(format: "%@: %.3f%%", file.path.components(separatedBy: "/").last!.padding(toLength: 50, withPad: " ", startingAt: 0), file.lineCoverage * 100)
+            if file.lineCoverage * 100 < 70 {
                 warn("Low test coverage " + finalMessage)
             } else {
                 message("Good test coverage " + finalMessage)
             }
+
         }
     }
 
